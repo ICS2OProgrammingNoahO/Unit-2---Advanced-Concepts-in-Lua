3af96195083afd81b8e730ce2ce4c6de8471a13a
Noah Ouellette <noah.ouellette@ocsbstudent.ca>
2019-11-13 14:30:45 -0500

    1

--- DraggingAnswersNoahO/level1_screen.lua
+++ DraggingAnswersNoahO/level1_screen.lua
@@ -37,7 +37,8 @@ local questionText
 --the alternate numbers randomly generated
 local correctAnswer
 local alternateAnswer1
-local alternateAnswer2    
+local alternateAnswer2 
+local alternateAnswer3   
 
 -- Variables containing the user answer and the actual answer
 local userAnswer
@@ -46,21 +47,28 @@ local userAnswer
 local answerboxAlreadyTouched = false
 local alternateAnswerBox1AlreadyTouched = false
 local alternateAnswerBox2AlreadyTouched = false
+local alternateAnswerBox3AlreadyTouched = false
+
 
 --create textboxes holding answer and alternate answers 
 local answerbox
 local alternateAnswerBox1
 local alternateAnswerBox2
+local alternateAnswerBox3
+
 
 -- create variables that will hold the previous x- and y-positions so that 
 -- each answer will return back to its previous position after it is moved
 local answerboxPreviousY
 local alternateAnswerBox1PreviousY
 local alternateAnswerBox2PreviousY
+local alternateAnswerBox3PreviousY
 
 local answerboxPreviousX
 local alternateAnswerBox1PreviousX
 local alternateAnswerBox2PreviousX
+local alternateAnswerBox3PreviousX
+
 
 -- the black box where the user will drag the answer
 local userAnswerBoxPlaceholder
@@ -94,6 +102,8 @@ local function DisplayQuestion()
     answerboxAlreadyTouched = false
     alternateAnswerBox1AlreadyTouched = false
     alternateAnswerBox2AlreadyTouched = false
+    alternateAnswerBox3AlreadyTouched = false
+
 
 end
 
@@ -108,6 +118,10 @@ local function DetermineAlternateAnswers()
     alternateAnswer2 = correctAnswer - math.random(1, 2)
     alternateAnswerBox2.text = alternateAnswer2
 
+    -- generate incorrect answer and set it in the textbox
+    alternateAnswer3 = correctAnswer - math.random(6, 8)
+    alternateAnswerBox3.text = alternateAnswer2
+
 -------------------------------------------------------------------------------------------
 -- RESET ALL X POSITIONS OF ANSWER BOXES (because the x-position is changed when it is
 -- placed into the black box)
@@ -115,6 +129,7 @@ local function DetermineAlternateAnswers()
     answerbox.x = display.contentWidth * 0.9
     alternateAnswerBox1.x = display.contentWidth * 0.9
     alternateAnswerBox2.x = display.contentWidth * 0.9
+    alternateAnswerBox3.x = display.contentWidth * 0.9
 
 
 end
@@ -133,6 +148,9 @@ local function PositionAnswers()
         answerbox.y = display.contentHeight * 0.4
 
         --alternateAnswerBox2
+        alternateAnswerBox3.y = display.contentHeight * 0.85
+
+        --alternateAnswerBox2
         alternateAnswerBox2.y = display.contentHeight * 0.70
 
         --alternateAnswerBox1
@@ -142,6 +160,8 @@ local function PositionAnswers()
         --remembering their positions to return the answer in case it's wrong
         alternateAnswerBox1PreviousY = alternateAnswerBox1.y
         alternateAnswerBox2PreviousY = alternateAnswerBox2.y
+        alternateAnswerBox3PreviousY = alternateAnswerBox3.y
+
         answerboxPreviousY = answerbox.y 
 
     -- random position 2
@@ -154,6 +174,8 @@ local function PositionAnswers()
 
         --alternateAnswerBox1
         alternateAnswerBox1.y = display.contentHeight * 0.7
+
+                alternateAnswerBox3.y = display.contentHeight * 0.85
 
         --remembering their positions to return the answer in case it's wrong
         alternateAnswerBox1PreviousY = alternateAnswerBox1.y
@@ -170,9 +192,13 @@ local function PositionAnswers()
         --alternateAnswerBox1
         alternateAnswerBox1.y = display.contentHeight * 0.4
 
+        alternateAnswerBox3.y = display.contentHeight * 0.85
+
         --remembering their positions to return the answer in case it's wrong
         alternateAnswerBox1PreviousY = alternateAnswerBox1.y
         alternateAnswerBox2PreviousY = alternateAnswerBox2.y
+        alternateAnswerBox3PreviousY = alternateAnswerBox3.y
+
         answerboxPreviousY = answerbox.y 
     end
 end
